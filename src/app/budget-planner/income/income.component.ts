c1343d425e7a699aa24b34645e4d66fe877cb878
Shruti Chandak <99483160+shruti-2412@users.noreply.github.com>
2024-09-08 13:20:18 +0530

    changed file directory

--- src/app/budget-planner/income/income.component.ts
+++ src/app/budget-planner/income/income.component.ts
@@ -12,6 +12,7 @@ export class IncomeComponent {
 
   incomeForm: any;
   selectedMonth: string;
+  monthSelected: boolean= false;
 
   januaryIncomes: any[] = [
     { source: 'Salary', amount: 5000, investments: '401(k)' },
